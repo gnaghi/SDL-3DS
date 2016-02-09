7f4450e20036480afd1f37c2ed2d924abe52eafb
James <gotnospirit@gmail.com>
2016-02-09 10:50:54 +0100

    fix to build with libctru v1.1.0

--- src/render/3ds/SDL_render_3ds.c
+++ src/render/3ds/SDL_render_3ds.c
@@ -467,8 +467,8 @@ N3DS_CreateRenderer(SDL_Window * window, Uint32 flags)
 	GPU_SetStencilOp(GPU_STENCIL_KEEP, GPU_STENCIL_KEEP, GPU_STENCIL_KEEP);
 	GPU_SetBlendingColor(0,0,0,0);
 	GPU_SetDepthTestAndWriteMask(true, GPU_GEQUAL, GPU_WRITE_ALL);
-	GPUCMD_AddMaskedWrite(GPUREG_0062, 0x1, 0);
-	GPUCMD_AddWrite(GPUREG_0118, 0);
+	GPUCMD_AddMaskedWrite(GPUREG_EARLYDEPTH_TEST1, 0x1, 0);
+	GPUCMD_AddWrite(GPUREG_EARLYDEPTH_TEST2, 0);
 
 	GPU_SetAlphaBlending(
 		GPU_BLEND_ADD,
@@ -486,7 +486,7 @@ N3DS_CreateRenderer(SDL_Window * window, Uint32 flags)
 	GPU_SetDummyTexEnv(5);
 
 	GPUCMD_Finalize();
-	GPUCMD_FlushAndRun(NULL);
+	GPUCMD_FlushAndRun();
 	gspWaitForP3D();
 
 	N3DS_pool_reset(data);
@@ -1052,7 +1052,7 @@ N3DS_RenderPresent(SDL_Renderer * renderer)
 
 	GPU_FinishDrawing();
 	GPUCMD_Finalize();
-	GPUCMD_FlushAndRun(NULL);
+	GPUCMD_FlushAndRun();
 	gspWaitForP3D();
 
 	//Copy the GPU rendered FB to the screen FB
@@ -1066,7 +1066,7 @@ N3DS_RenderPresent(SDL_Renderer * renderer)
 	/* Swap buffers */
 	gfxSwapBuffersGpu();
 	if (data->vsync) {
-		gspWaitForEvent(GSPEVENT_VBlank0, true);
+		gspWaitForEvent(GSPGPU_EVENT_VBlank0, true);
 	}
 
 	//sceGuFinish();
