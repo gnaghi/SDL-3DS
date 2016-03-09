bbc924537f42b4881ce9bf3dbeb3edbc4e8c92aa
James <gotnospirit@gmail.com>
2016-03-09 12:00:12 +0100

    keyboard events++

--- src/video/3ds/SDL_3dsevents.c
+++ src/video/3ds/SDL_3dsevents.c
@@ -28,88 +28,66 @@
 #include "SDL.h"
 #include "../../events/SDL_sysevents.h"
 #include "../../events/SDL_events_c.h"
-#include "../../events/SDL_keyboard_c.h"
+
+#include <3ds.h>
 #include "SDL_3dsvideo.h"
 #include "SDL_3dsevents_c.h"
-#include "SDL_thread.h"
-#include "SDL_keyboard.h"
-//#include <psphprm.h>
 
-
-//static enum PspHprmKeys hprm = 0;
-static SDL_sem *event_sem = NULL;
-static SDL_Thread *thread = NULL;
-static int running = 0;
-
-int EventUpdate(void *data)
-{
-    while (running) {
-                SDL_SemWait(event_sem);
-		//sceHprmPeekCurrentKey(&hprm);
-                SDL_SemPost(event_sem);
-                /* Delay 1/60th of a second */
-                //sceKernelDelayThread(1000000 / 60); 3DS STUB
-        }
-        return 0;
-}
-
+static struct {
+    int id;
+    SDL_Keycode sym;
+} keymap[] = {
+    { KEY_A, SDLK_a },
+    { KEY_B, SDLK_b },
+    { KEY_R, SDLK_r },
+    { KEY_L, SDLK_l },
+    { KEY_X, SDLK_x },
+    { KEY_Y, SDLK_y },
+    { KEY_SELECT, SDLK_SELECT },
+    { KEY_START, SDLK_MENU },
+    { KEY_DRIGHT, SDLK_RIGHT },
+    { KEY_DLEFT, SDLK_LEFT },
+    { KEY_DUP, SDLK_UP },
+    { KEY_DDOWN, SDLK_DOWN }
+};
 
 void N3DS_PumpEvents(_THIS)
 {
-#if 0
-    int i;
-    enum PspHprmKeys keys;
-    enum PspHprmKeys changed;
-    static enum PspHprmKeys old_keys = 0;
-    SDL_Keysym sym;
+    u32 keys;
+    int max = sizeof(keymap) / sizeof(keymap[0]);
 
-    SDL_SemWait(event_sem);
-    keys = hprm;
-    SDL_SemPost(event_sem);
+    //Scan all the inputs. This should be done once for each frame
+    hidScanInput();
 
-    /* HPRM Keyboard */
-    changed = old_keys ^ keys;
-    old_keys = keys;
-    if(changed) {
-        for(i=0; i<sizeof(keymap_psp)/sizeof(keymap_psp[0]); i++) {
-            if(changed & keymap_psp[i].id) {
-                sym.scancode = keymap_psp[i].id;
-                sym.sym = keymap_psp[i].sym;
-
-                /* out of date
-                SDL_PrivateKeyboard((keys & keymap_psp[i].id) ?
-                            SDL_PRESSED : SDL_RELEASED,
-                            &sym);
-        */
-                SDL_SendKeyboardKey((keys & keymap_psp[i].id) ?
-                                    SDL_PRESSED : SDL_RELEASED, SDL_GetScancodeFromKey(keymap_psp[i].sym));
-            }
+    //hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
+    keys = hidKeysDown();
+    for (int i = 0; i < max; ++i)
+    {
+        if (keys & keymap[i].id)
+        {
+            SDL_SendKeyboardKey(SDL_PRESSED, SDL_GetScancodeFromKey(keymap[i].sym));
         }
     }
-#endif
+
+    keys = hidKeysUp();
+    for (int i = 0; i < max; ++i)
+    {
+        if (keys & keymap[i].id)
+        {
+            SDL_SendKeyboardKey(SDL_RELEASED, SDL_GetScancodeFromKey(keymap[i].sym));
+        }
+    }
     return;
 }
 
-
 void N3DS_EventInit(_THIS)
 {
-    /* Start thread to read data */
-    if((event_sem =  SDL_CreateSemaphore(1)) == NULL) {
-        SDL_SetError("Can't create input semaphore\n");
-        return;
-    }
-    running = 1;
-    if((thread = SDL_CreateThread(EventUpdate, "3DSInputThread",NULL)) == NULL) {
-        SDL_SetError("Can't create input thread\n");
-        return;
-    }
+    hidInit();
 }
 
 void N3DS_EventQuit(_THIS)
 {
-    running = 0;
-    SDL_WaitThread(thread, NULL);
-    SDL_DestroySemaphore(event_sem);
+    hidExit();
 }
 
 /* end of SDL_3dsevents.c ... */
